1cbea81e642ab3809ec58c02d3afe764dceec27e
Edward Fernandez <edanfesi@gmail.com>
2019-08-19 21:22:46 -0400

    Removed one of the links of the navbar and updated the style for the Header component.
    I remove the Project link because I don't have enough projects to show

--- portfolio-website/src/components/header.js
+++ portfolio-website/src/components/header.js
@@ -9,21 +9,23 @@ class Header extends Component {
       <header className="header">
         <div className="header-wrapper">
           <div className="logo">
-            Edward<span>F</span>.
+            <Link to="/">
+              Edward<span>F</span>.
+            </Link>
           </div>
 
           <nav className="nav">
             <ul>
               <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="#">Projects</Link>
+                <Link to="/about/">About</Link>
               </li>
             </ul>
           </nav>
 
-          <a className="btn" href="#">
+          <a
+            className="btn"
+            href="mailto:edanfesi@gmail.com?subject=Hello%20Edward"
+          >
             Contact me
           </a>
         </div>
