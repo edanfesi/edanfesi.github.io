1160bbc4c2e0d7a730268abe8ec7e1af1a739390
Edward Fernandez <edanfesi@gmail.com>
2019-08-19 21:05:13 -0400

    Updated the hero style

--- portfolio-website/src/components/hero.js
+++ portfolio-website/src/components/hero.js
@@ -6,9 +6,9 @@ import ProfileContact from "./profile_contact"
 
 function Hero() {
   return (
-    <main className="section">
+    <section className="section">
       <div className="hero-wrapper">
-        <span className="hero__icon" role="img" arial-label="Waving Hand">
+        <span className="hero__icon" role="img" aria-label="Waving Hand">
           👋
         </span>
 
@@ -18,16 +18,16 @@ function Hero() {
 
         <p className="hero__description">
           Currently developing, integrating and testing end-to-end solution
-          implementation services at <a href="#">EY Panama</a> to help companies
-          thrive in the transformative age.
+          implementation services at{" "}
+          <a href="https://www.ey.com/ac/en/home">EY Panama</a> to help
+          companies thrive in the transformative age.
         </p>
 
         <button className="btn hero__btn">Get Started</button>
 
         <ProfileContact />
       </div>
-      <hr />
-    </main>
+    </section>
   )
 }
 
